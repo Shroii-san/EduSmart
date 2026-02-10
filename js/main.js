96f0a8121f1e03c6fe5ee0cd31b15b0fa63884bc
Izy <rzalwira23@gmail.com>
2026-02-11 01:37:44 +0700

    rapihin landing sampai home

--- js/main.js
+++ js/main.js
@@ -10,3 +10,34 @@ document.querySelectorAll(".menu-card").forEach(card => {
     card.style.transform = "scale(1)";
   });
 });
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("sidebarOverlay");
+
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+function closeSidebar() {
+  document.getElementById("sidebar").classList.remove("active");
+  document.getElementById("sidebarOverlay").classList.remove("active");
+}
+
+const navbar = document.getElementById("landingNavbar");
+const mobileMenu = document.getElementById("landingMobileMenu");
+
+function toggleLandingMenu() {
+  mobileMenu.classList.toggle("active");
+}
+
+/* Navbar transparency on scroll */
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    navbar.classList.add("solid");
+    navbar.classList.remove("transparent");
+  } else {
+    navbar.classList.add("transparent");
+    navbar.classList.remove("solid");
+  }
+});
